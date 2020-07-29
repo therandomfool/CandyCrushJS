3b151b984508b80eca7ab9e4cd63631c9d9beaeb
therandomfool <mddenver@protonmail.com>
2020-07-28 22:51:54 -0700

    check row for three and chack coloumn for three working

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
    const grid = document.querySelector('.grid')
    const width = 8;
    const squares = [];
+   let score = 0
 
 
 
@@ -92,8 +93,58 @@ function dragEnd() {
 
     let validMove = validMoves.includes(squareIdBeingReplaced)
 
-    if (squareIdBeingReplaced && valid)
+    if (squareIdBeingReplaced && validMove) {
+        squareIdBeingReplaced = null;
+
+    } else if (squareIdBeingReplaced && !validMove) {
+        squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced
+        squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+    } else {
+        squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+    }
 }
+
+//  Checking for matches
+
+//  checking for row of three
+function checkRowForThree() {
+    for(i=0; i < 61; i++) {
+        let rowOfThree = [i, i+1, i+2]
+        let decidedColor = squares[i].style.backgroundColor
+        const isBlank = squares[i].style.backgroundColor === ''
+
+        if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+            score += 3
+            rowOfThree.forEach(index => {
+                squares[index].style.backgroundColor= ''
+            })
+        }
+    }
+}
+checkRowForThree()
+
+//  checking for column of three
+function checkColumnForThree() {
+    for(i=0; i < 47; i++) {
+        let columnOfThree = [i, i+width, i+width*2]
+        let decidedColor = squares[i].style.backgroundColor
+        const isBlank = squares[i].style.backgroundColor === ''
+
+        if (columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+            score += 3
+            columnOfThree.forEach(index => {
+                squares[index].style.backgroundColor= ''
+            })
+        }
+    }
+}
+checkColumnForThree()
+
+window.setInterval(function(){
+    checkRowForThree()  
+    checkColumnForThree()
+}, 100);
+
 
 })
 
